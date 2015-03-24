081c445ee6a162497b651767f089183309bd8c69
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-03-24 23:17:19 +0900

    Fix attemt 2 for aws

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,7 +4,13 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, 
             :styles => { :medium => "300x300>", :thumb => "100x100>" },
             :storage => :s3,
-            :bucket  => ENV['MY_BUCKET_NAME']
+            :bucket  => ENV['MY_BUCKET_NAME'],
+            :s3_credentials => {
+                    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                },
+                :s3_permissions => 'private',
+                :url => ":s3_domain_url"
 
 	validates :image, presence: true
 	validates :description, presence: true
