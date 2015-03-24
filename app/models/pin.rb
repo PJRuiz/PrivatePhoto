e156b293d5feea418a25f7a3036a1d92ca06e41b
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-03-24 23:11:06 +0900

    Explicitly defined the bucket association in the Pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,10 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, 
+            :styles => { :medium => "300x300>", :thumb => "100x100>" },
+            :storage => :s3,
+            :bucket  => ENV['MY_BUCKET_NAME']
 
 	validates :image, presence: true
 	validates :description, presence: true
