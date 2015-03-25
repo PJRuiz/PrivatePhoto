e15ca67ef9323650ba3fc1b005682b37dbc1e674
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-03-25 16:09:17 +0900

    Changed to s3 on pin.rb as well

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,7 +4,7 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, 
             :styles => { :medium => "300x300>", :thumb => "100x100>" },
             :storage => :s3,
-            :bucket  => ENV['MY_BUCKET_NAME'],
+            :bucket  => ENV['S3_BUCKET_NAME'],
             :s3_credentials => {
                     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
                     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
